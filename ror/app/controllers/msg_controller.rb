cf018fa951b5b34148a083f37ddb72dd2097724b
takagotch <dyaccb@gmail.com>
2018-01-05 19:47:15 +0900

    describe>let,before, context>it;

--- ror/app/controllers/msg_controller.rb
+++ ror/app/controllers/msg_controller.rb
@@ -1,8 +1,14 @@
 def create
  #Msg.new(params[:msg])
  #redirect_to :action => "index"
-  message = Message.new(params[:message])
-  message.save
-  redirect_to :action => "index"
+  msg = Msg.new(params[:msg])
+#  message.save
+  if message.save
+    flash[:notice] = "The msg was saved successfully."
+    redirect_to :action => "index"
+  else
+	  render :action => "new"
+  end
+#  redirect_to :action => "index"
 end
 
--- ror/spec/controllers/msg_controller_spec.rb
+++ ror/spec/controllers/msg_controller_spec.rb
@@ -5,8 +5,11 @@ describe MessagesController do
 	let(:msg) { mock_model(Msg).as_null_object }
 
 	before do 
-	  Msg.stub(:new).and_return(msg)
+	 Msg.stub(:new).and_return(msg)
+	 #Msg.stub(:new).and_return(false)
+	 #Msg.stub(:new).and_return(true)
 	end
+
 
     it "create a new msg" do
      #pending("drive out redirect")   
@@ -33,6 +36,42 @@ describe MessagesController do
 #	and_return(message)
 #      post :create, :message => { "text" => "a quick brown fox" }
 #    end
+
+    context "when the msg saves successfully" do
+
+	before do 
+	  Msg.stub(:new).and_return(true)
+	end
+
+	    it "set a flash[:notice] message" do
+		    #msg.stub(:save).and_return(true)
+		    post :create
+	    flash[:notice].should eq("The msg was saved successfully.")
+	  end
+
+	  it "redirect to the Message index" do
+		  #msg.stub(:save).and_return(true)
+		  post :create
+		  response.should redirect_to(:action => "index")
+	  end
+	end
+  end
+
+  context "when the msg fails to save" do
+	before do 
+	  Msg.stub(:new).and_return(true)
+	end
+
+	 it "assign @msg" do
+     #msg.stub(:save).and_return(false)
+      post :create
+      assigns[:msg].should eq(msg)
+    end
+    it "renders the new template" do
+     #msg.stub(:save).and_return(fasle)
+      post :create
+      response.should render_template("new")
+    end
   end
 end
 
